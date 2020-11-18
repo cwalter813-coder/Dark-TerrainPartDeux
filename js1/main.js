8efe8b824c9403596c83a437bc736093fd48df67
cwalter813-coder <71605997+cwalter813-coder@users.noreply.github.com>
2020-11-18 09:42:05 -0600

    My 3rd Attempt at my 1st Commit

---
+++ js1/main.js
@@ -0,0 +1,50 @@
+//function to start the Leaflet map
+function createMap(){
+
+    //Varibles for attribution box on the map.
+    var mbAttr = '<a href="http://openstreetmap.org">OpenStreetMap</a> |' +' <a href="http://mapbox.com">Mapbox</a>'; //You can add you name to the variable if you want to add yourself to the credits.
+    
+    //Variable for storing your Mapbox API Token
+    var apitoken = 'pk.eyJ1IjoiY29yeS13YWx0ZXIiLCJhIjoiY2tobTVuaGJpMGs0ZTJ2bmthMGplZjFkaSJ9._l00ok2lOgOU1K4gx3z9sg' //Enter your API Token - go to 'https://www.mapbox.com/install/' to yours */
+                    
+     //URL used for Stanard MaxBox Styles
+    var mbUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={token}';
+    
+    //URL used for Custom MapBox Style
+    var mbStyleUrl = 'https://api.mapbox.com/styles/v1/{id}/tiles/256/{z}/{x}/{y}?access_token={token}'; s
+    
+    //For Custome basemaps - Replace your username and StyleID
+    var customeBasemap = L.tileLayer(mbStyleUrl, {id: 'cory-walter/ckhmt0eh9077r19luge8ms28o', token: apitoken,  attribution: mbAttr});
+    
+    //For MabBox Standard Basemaps
+    var standardBasemap   = L.tileLayer(mbUrl, {id: 'mapbox.light', token: apitoken, attribution: mbAttr});
+    
+    //Variables for the different basemaps
+    var darkTerrain = L.tileLayer(mbStyleUrl, {id: 'cory-walter/ckhmt0eh9077r19luge8ms28o', token: apitoken,  attribution: mbAttr});   
+    
+    var grayscale   = L.tileLayer(mbUrl, {id: 'mapbox.light', token: apitoken, attribution: mbAttr});
+    
+    var dark  = L.tileLayer(mbUrl, {id: 'mapbox.dark', token: apitoken,  attribution: mbAttr});
+    
+    var outdoors = L.tileLayer(mbUrl, {id: 'mapbox.outdoors', token: apitoken,  attribution: mbAttr});
+    
+    //create the map*/
+    var map = L.map('map', {
+        center: [47.152, -122.578],//Coordinated to center the map
+        zoom: 6, //zoom level
+        layers:customeBasemap //basemap
+    });
+    
+    //create the basemap control layer*/
+    var baseLayers = {
+		"Outdoors": outdoors,
+        "Grayscale": grayscale,
+		"Darkscale": dark,
+        "Dark Terrain": darkTerrain
+    };
+    
+    L.control.layers(baseLayers).addTo(map);
+};
+
+//calling the funcation
+$(document).ready(createMap);
